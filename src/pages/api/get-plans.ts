cf976e50469b8b279606be7f43e04491c7862122
Igor Stefano <igstefano90@live.com>
2024-03-11 00:08:22 -0300

    fix(get-plans): fix API endpoint for getting plans

--- src/pages/api/get-plans.ts
+++ src/pages/api/get-plans.ts
@@ -1,20 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { promises as fs } from "fs";
-import { PlanData, PlanSchema } from "@/components/form/types/plans";
+import { PlanAPISchema, PlanDataAPI } from "@/components/form/types/plans";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<PlanData[]>
+  res: NextApiResponse,
 ) {
   const file = await fs.readFile(process.cwd() + "/plans.json", "utf8");
-  const data: PlanData[] = JSON.parse(file);
+  const data: PlanDataAPI[] = JSON.parse(file);
 
-  const result = PlanSchema.safeParse(data);
+  const result = data.filter((item) => {
+    const parse = PlanAPISchema.safeParse(item);
+    return parse.success;
+  });
 
-  if (result.success) {
+  if (result.length > 0) {
     res.status(200).json(data);
   } else {
-    res.status(500);
+    res.status(500).json({
+      code: 500,
+      message: "Failure to get Plans data",
+    });
   }
 }
